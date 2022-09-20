d40ccdf7a1f4c4066fbed8b4c1d765c6ae320edf
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-20 13:53:27 +0200

    Files created

---
+++ src/Ember.java
@@ -0,0 +1,20 @@
+public class Ember {
+    private String nev;
+    private String szulDatum;
+    private String szulHely;
+
+    public Ember(String nev, String szulDatum, String szulHely) {
+        this.nev = nev;
+        this.szulDatum = szulDatum;
+        this.szulHely = szulHely;
+    }
+
+    @Override
+    public String toString() {
+        return "Ember{" +
+                "nev='" + nev + '\'' +
+                ", szulDatum='" + szulDatum + '\'' +
+                ", szulHely='" + szulHely + '\'' +
+                '}';
+    }
+}
