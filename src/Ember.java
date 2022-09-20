4d88f657d78b71395ab99fffb440a53a9986e85a
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-20 14:21:33 +0200

    String with Splits

--- src/Ember.java
+++ src/Ember.java
@@ -11,10 +11,18 @@ public class Ember {
 
     @Override
     public String toString() {
-        return String.format("%30s %30s %30s", this.nev, this.szulDatum, this.szulHely);
+        return String.format("%30s %30s %30s", this.nev, this.szulDatum, this.szulHely, this.getSzuletesiEv());
     }
     public int getSzuletesiEv() {
         return Integer.parseInt(szulDatum.substring(0, 4));
 
     }
+    public int getSzuletesiHonap(){
+        return Integer.parseInt(szulDatum.split("-")[1]);
+    }
+
+    public int getSzuletesiNap(){
+        return Integer.parseInt(szulDatum.split("-")[2]);
+    }
+
 }
