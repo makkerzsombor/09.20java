b179a759fbb28a8dd36d97b2ec4d5c223ddc3d37
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-20 14:00:46 +0200

    Eore ember called into main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Ember human = new Ember("Gipsz Jakab","2001-1-1","Budapest");
+        Ember human2 = new Ember("Teszt Elek","1985-12-15","Debrecen");
+        Ember human3 = new Ember("Kovács István","1999-3-30","Szeged");
         System.out.println(human);
+        System.out.println(human2);
+        System.out.println(human3);
     }
 }
