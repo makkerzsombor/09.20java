4eacf0cc4994f2802cf77a1feb09083a6334340d
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-20 15:25:43 +0200

    Emberek read file

--- src/Emberek/Emberek.java
+++ src/Emberek/Emberek.java
@@ -1,11 +1,40 @@
 package Emberek;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
 public class Emberek {
-    private List<Ember> emberek;
+    private List<Ember> emberek = null;
 
-    public Emberek(){
+    public Emberek(Ember[] emberTomb){
+        this.emberek = new ArrayList<>();
+        this.emberek.addAll(Arrays.asList(emberTomb));
+    }
 
+    public Emberek(String fajlNev) throws IOException {
+        this.emberek = new ArrayList<>();
+        FileReader fr = new FileReader(fajlNev);
+        BufferedReader br = new BufferedReader(fr);
+        String sor = br.readLine();
+        while(sor != null && !sor.equals("")){
+            String[] adatok = sor.split(";");
+            Ember ember = new Ember(adatok[0], adatok[1], adatok[2]);
+            this.emberek.add(ember);
+            sor=br.readLine();
+        }
+        fr.close();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Ember e:this.emberek) {sb.append(e.toString()).append(System.lineSeparator());
+
+        }
+        return sb.toString();
     }
 }
--- src/Emberek/Main.java
+++ src/Emberek/Main.java
@@ -1,12 +1,24 @@
 package Emberek;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Ember human = new Ember("Gipsz Jakab","2001-1-1","Budapest");
         Ember human2 = new Ember("Teszt Elek","1985-12-15","Debrecen");
         Ember human3 = new Ember("Kovács István","1999-3-30","Szeged");
-        System.out.println(human);
-        System.out.println(human2);
-        System.out.println(human3);
+        Ember human4 = new Ember("Kovács Roland","1989-3-30","Szekszárd");
+        Ember human5 = new Ember("Nagy István","1969-3-30","Debrecen");
+        Emberek emberek = new Emberek(new Ember[]{human, human2, human3, human4, human5});
+        //System.out.println(emberek);
+        String fajlNev = "emberek.txt";
+        try {
+            Emberek emberek2 = new Emberek(fajlNev);
+        } catch (FileNotFoundException e) {
+            System.err.printf("Hiba nem található a(z) %s fájl\n", fajlNev);
+        } catch (IOException e){
+            System.err.println("Ismeretlen hiba történt a fájl beolvasása során.");
+        }
     }
 }
